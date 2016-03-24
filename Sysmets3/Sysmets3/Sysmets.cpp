550ac437e7bc1e245015aaad6f3076653e3a96fa
Magicmay <634665383@qq.com>
2016-03-24 20:50:28 +0800

    “系统文件测试3”

---
+++ Sysmets3/Sysmets3/Sysmets.cpp
@@ -0,0 +1,238 @@
+#include <windows.h>
+#include "Sysmets.h"
+#pragma comment(lib,"winmm.lib")
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+	//HINSTANCE 执行实体（程序自身）代号
+	//HWND视窗代号
+	//HDC装置代号
+	//MSG 讯息结构
+	//WNDCLASS 视窗类别结构
+	//PAINTSTRUCT 矩形结构
+	static TCHAR szAppName[] = TEXT("SysMets3");
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS wndclass;
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = szAppName;
+
+	if (!RegisterClass(&wndclass))
+	{
+		MessageBox(NULL, TEXT("this prigram requires Windows 7!"), szAppName, MB_ICONERROR);
+		return 0;
+	}
+	hwnd = CreateWindow(szAppName,//window class name
+		TEXT("Get System Metrics No.3"),//window caption
+		WS_OVERLAPPEDWINDOW | WS_VSCROLL|WS_HSCROLL,//window style
+		100,//initial x position
+		100,//initial y position
+		640,//initial x size
+		480,//initial y size
+		NULL,//parent window handle
+		NULL,//window menu handle
+		hInstance,//program instance handle
+		NULL//creation parameters
+		);
+	ShowWindow(hwnd, iCmdShow);//显示
+	UpdateWindow(hwnd);//重画
+	while (GetMessage(&msg, NULL, 0, 0))//讯息回圈
+	{
+		TranslateMessage(&msg);//键盘转换
+		DispatchMessage(&msg);//呼叫WndProc,处理完后传给getMessage,循环。
+	}
+	return msg.wParam;
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)//视窗讯息处理程式
+{
+	//CS_视窗类别样式
+	//CW_建立视窗
+	//DT_绘制文字
+	//IDI_图示ID
+	//IDC_游标ID
+	//MB_讯息方块
+	//SND_声音
+	//WM_视窗讯息
+	//WS_视窗样式
+	
+	
+	static int  cxChar, cxCaps, cyChar, cxClient, cyClient, iMaxWidth;
+	HDC hdc;
+	int i, x, y, iVertPos, iHorzPos, iPaintBeg, iPaintEnd;
+	PAINTSTRUCT ps;
+	SCROLLINFO si;
+	TCHAR szBuffer[10];
+	TEXTMETRIC tm;
+	switch (message)
+	{
+	case WM_CREATE:
+		hdc = GetDC(hwnd);
+
+		GetTextMetrics(hdc, &tm);
+		cxChar = tm.tmAveCharWidth;
+		cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2)*cxChar / 2;
+		cyChar = tm.tmHeight + tm.tmExternalLeading;
+		ReleaseDC(hwnd, hdc);
+		// Save the width of the three columns
+		iMaxWidth = 40 * cxChar + 22 * cxCaps;
+		
+		return 0;
+	case WM_SIZE:
+		cxClient = LOWORD(lParam);
+		cyClient = HIWORD(lParam);
+
+		// Set vertical scroll bar range and page size
+		si.cbSize = sizeof(si);
+		si.fMask = SIF_RANGE | SIF_PAGE;
+		si.nMin = 0;
+		si.nMax = NUMLINES - 1;
+		si.nPage = cyClient / cyChar;
+		SetScrollInfo(hwnd, SB_VERT, &si, TRUE);
+
+		// Set horizontal scroll bar range and page size
+		si.cbSize = sizeof(si);
+		si.fMask = SIF_RANGE | SIF_PAGE;
+		si.nMin = 0;
+		si.nMax = 2+iMaxWidth/cxChar;
+		si.nPage = cxClient / cxChar;
+		SetScrollInfo(hwnd, SB_HORZ, &si, TRUE);
+		return 0;
+		
+	case WM_VSCROLL:
+		// Get all the vertical scroll bar information
+		si.cbSize = sizeof(si);
+		si.fMask = SIF_ALL;
+		GetScrollInfo(hwnd, SB_VERT, &si);
+		// Save the position for comparison later on
+		iVertPos = si.nPos;
+		switch (LOWORD(wParam))
+		{
+		case SB_TOP:
+			si.nPos = si.nMin;
+			break;
+		case SB_BOTTOM:
+			si.nPos = si.nMax;
+			break;
+		case SB_LINEUP:
+			si.nPos -= 1;
+			break;;
+		case SB_LINEDOWN:
+			si.nPos += 1;
+			break;
+		case SB_PAGEUP:
+			si.nPos -=si.nPage;
+			break;
+		case SB_PAGEDOWN:
+			si.nPos += si.nPage;
+			break;
+		case SB_THUMBTRACK:
+			si.nPos = si.nTrackPos;
+			break;
+	
+		default:
+			break;
+		}
+		// Set the position and then retrieve it. Due to adjustments
+		// by Windows it may not be the same as the value set.
+		si.fMask = SIF_POS;
+		SetScrollInfo(hwnd, SB_VERT, &si, TRUE);
+		GetScrollInfo(hwnd, SB_VERT, &si);
+		// If the position has changed, scroll the window and update it
+		if (si.nPos != iVertPos)
+		{
+			ScrollWindow(hwnd, 0, cyChar*(iVertPos - si.nPos), NULL, NULL);
+			UpdateWindow(hwnd);
+		}
+		return 0;
+	case WM_HSCROLL:
+		// Get all the vertical scroll bar information
+		si.cbSize = sizeof(si);
+		si.fMask = SIF_ALL;
+		GetScrollInfo(hwnd, SB_HORZ, &si);
+		// Save the position for comparison later on
+		iHorzPos = si.nPos;
+		switch (LOWORD(wParam))
+		{
+		case SB_LINELEFT:
+			si.nPos -= 1;
+			break;;
+		case SB_LINERIGHT:
+			si.nPos += 1;
+			break;
+		case SB_PAGELEFT:
+			si.nPos -= si.nPage;
+			break;
+		case SB_PAGERIGHT:
+			si.nPos += si.nPage;
+			break;
+		case SB_THUMBPOSITION:
+			si.nPos = si.nTrackPos;
+			break;
+		default:
+			break;
+		}
+		// Set the position and then retrieve it. Due to adjustments
+		// by Windows it may not be the same as the value set.
+		si.fMask = SIF_POS;
+		SetScrollInfo(hwnd, SB_HORZ, &si, TRUE);
+		GetScrollInfo(hwnd, SB_HORZ, &si);
+		// If the position has changed, scroll the window and update it
+		if (si.nPos != iHorzPos)
+		{
+			ScrollWindow(hwnd, cxChar*(iHorzPos-si.nPos), 0, NULL, NULL);
+		}
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		// Get vertical scroll bar position
+		si.cbSize = sizeof(si);
+		si.fMask = SIF_POS;
+		GetScrollInfo(hwnd, SB_VERT, &si);
+		iVertPos = si.nPos;
+		// Get horizontal scroll bar position
+		GetScrollInfo(hwnd, SB_HORZ, &si);
+		iHorzPos = si.nPos;
+		// Find painting limits
+		iPaintBeg = max(0, iVertPos + ps.rcPaint.top / cyChar);
+		iPaintEnd = min(NUMLINES - 1, iVertPos + ps.rcPaint.bottom / cyChar);
+
+		for (i = iPaintBeg; i <=iPaintEnd; i++)
+		{
+			x = cxChar * (1 - iHorzPos);
+			y = cyChar * (i - iVertPos);
+
+			TextOut(hdc, x, y,
+				sysmetrics[i].szLabel,
+				lstrlen(sysmetrics[i].szLabel));
+
+			TextOut(hdc, x + 22 * cxCaps, y,
+				sysmetrics[i].szDesc,
+				lstrlen(sysmetrics[i].szDesc));
+
+			SetTextAlign(hdc, TA_RIGHT | TA_TOP);
+
+			TextOut(hdc, x + 22 * cxCaps + 40 * cxChar, y, szBuffer,
+				wsprintf(szBuffer, TEXT("%5d"),
+				GetSystemMetrics(sysmetrics[i].Index)));
+
+			SetTextAlign(hdc, TA_LEFT | TA_TOP);
+		}
+		
+		EndPaint(hwnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	//视窗讯息处理程式不予处理的所有讯息提供内定处理
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
